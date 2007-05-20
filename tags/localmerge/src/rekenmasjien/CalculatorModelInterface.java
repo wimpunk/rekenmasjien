8505a21bf53b044a653ff5aba8f9daa5ed2a854d
wimpunk <wimpunk@c67bc7d4-e630-0410-adf4-93be5ccf8126>
2007-05-20 15:58:00 +0000

    Merged with local repository (more or less)

---
+++ tags/localmerge/src/rekenmasjien/CalculatorModelInterface.java
@@ -0,0 +1,16 @@
+package rekenmasjien;
+/*
+ * $Id$
+ *
+ * $LastChangedDate$
+ * $Rev$
+ * $Author$
+ *
+ * */
+public interface CalculatorModelInterface {
+
+	public abstract Integer getGetal();
+
+	public abstract void setC(char c);
+
+}
